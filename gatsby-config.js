93b28da3fdea5179ff591c5c0e72492f967ebc74
Dhwaneet Bhatt <dhwaneetbhatt@gmail.com>
2021-07-08 16:40:35 +0530

    Updates on Postman (#1)
    * Updates about postman
    
    * Updates for postman
    Section on books
    
    * Replacing yarn with npm

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,12 +28,16 @@ module.exports = {
         formatString: 'MMM DD, YYYY',
         navigation: [
           {
-            title: `Blog`,
+            title: `🖊️ Blog`,
             slug: `/blog`,
           },
           {
-            title: `About`,
+            title: `🙋‍♂️ About`,
             slug: `/about`,
+          },
+          {
+            title: `📚 Books`,
+            slug: `/books`,
           },
         ],
         externalLinks: [
@@ -45,42 +49,49 @@ module.exports = {
             name: `LinkedIn`,
             url: `https://www.linkedin.com/in/dhwaneetbhatt`,
           },
+          {
+            name: `GitHub`,
+            url: `https://github.com/dhwaneetbhatt`,
+          },
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
+        {
+      resolve: `gatsby-omni-font-loader`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        enableListener: true,
+        preconnect: [`https://fonts.gstatic.com`],
+        interval: 300,
+        timeout: 30000,
+        web: [
+          {
+            name: `IBM Plex Sans`,
+            file: `https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap`,
+          },
+        ],
       },
     },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Dhwaneet Bhatt's Blog`,
+        short_name: `dhwaneet-bhatt`,
+        description: `About Dhwaneet Bhatt`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
+            src: `/source-code-icon-152-213242.png`,
+            sizes: `152x152`,
             type: `image/png`,
           },
         ],
       },
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-netlify`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
