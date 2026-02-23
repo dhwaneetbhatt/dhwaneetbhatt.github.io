fe367d96803deb2f1009fb6028662bafa20b0c97
dhwaneetbhatt <dhwaneetbhatt@gmail.com>
2026-02-23 10:45:39 +0000

    Blog post for joining ignosis

--- content/pages/books/goodreads.jsx
+++ content/pages/books/goodreads.jsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 function BooksComponent () {
   React.useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://www.goodreads.com/review/grid_widget/109274211.My%20Read%20Books?cover_size=medium&num_books=50&order=d&shelf=read&sort=date_read&widget_id=1624612170';
+    script.src = 'https://www.goodreads.com/review/grid_widget/109274211.My%20Read%20Books?cover_size=medium&num_books=100&order=d&shelf=read&sort=date_read&widget_id=1624612170';
     script.async = true;
     document.body.appendChild(script);
   })
